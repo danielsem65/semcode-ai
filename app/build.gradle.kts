b2cac8d331c198bd337197ccfaa0a6f4e0826c1f
HostWebs <maurasmith254@gmail.com>
2026-08-22 06:33:07 +0000

    bump versionCode 17 / 2.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.danielsem65.semcodeai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "2.4.1"
+        versionCode = 17
+        versionName = "2.4.2"
     }
 
     buildTypes {
