34de9bde7bc92ca3a67281549d9a31e3fc0fa489
HostWebs <maurasmith254@gmail.com>
2026-09-13 07:02:11 +0000

    Bump to 2.17.0 (build 43)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         // exec() from app storage is allowed (required for the proot Linux env)
         // and enables classic full-storage permissions.
         targetSdk = 28
-        versionCode = 42
-        versionName = "2.16.0"
+        versionCode = 43
+        versionName = "2.17.0"
     }
 
     signingConfigs {
